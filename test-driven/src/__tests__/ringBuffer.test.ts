73cf7b2fe1103a213c83b2510a59b514f163c8b4
EBIHARA Kenji <kenji.ebihara@jp.ricoh.com>
2021-12-14 10:23:15 +0900

    テスト駆動開発を丁寧に説明するようにした （途中）

--- test-driven/src/__tests__/ringBuffer.test.ts
+++ test-driven/src/__tests__/ringBuffer.test.ts
@@ -1,13 +1,21 @@
-/*
-+ バッファの大きさを指定してバッファを作成する
-- 最新のバッファを取得する
-- 最新から指定した数を戻ったインデックスの値を取得する
-- length は、バッファの大きさを返す
+/* TODO
+✔ 大きさを指定してバッファを作成する
+- バッファから最新の値を取得する
+- バッファから最新から指定した数を戻ったインデックスの値を取得する
+- バッファに値を追加する
+- buffer を非公開にする
 */
 
 import { RingBuffer } from '../ringBuffer';
 
-test('バッファの大きさを指定して作成する', () => {
-  const buffer = new RingBuffer(10);
-  expect(buffer.length).toBe(10);
+test('バッファの大きさを指定してバッファを作成する', () => {
+  const ringBuffer = new RingBuffer(10);
+  expect(ringBuffer.buffer.length).toBe(10);
+});
+
+test('バッファから最新の値を取得する', () => {
+  const ringBuffer = new RingBuffer(5);
+  ringBuffer.push('a1');
+  ringBuffer.push('a2');
+  expect(ringBuffer.getCurrent()).toBe('a2');
 });
--- test-driven/src/ringBuffer.ts
+++ test-driven/src/ringBuffer.ts
@@ -1,6 +1,13 @@
 export class RingBuffer {
-  constructor(size: number) {
-    this.length = size;
+  public constructor(size: number) {
+    this.buffer = Array(size);
   }
-  public length: number;
+  public buffer;
+  public push(value: any) {
+    this.buffer[0] = 'a2';
+  }
+  public getCurrent() {
+    return this.buffer[this.currentIndex];
+  }
+  private currentIndex = 0;
 }
