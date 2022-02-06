85cf81630e8956a9968020399fb8074812be7d95
EBIHARA Kenji <kenji.ebihara@jp.ricoh.com>
2022-02-07 08:24:16 +0900

    誤記等の修正

--- async-functions/src/index.tsx
+++ async-functions/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { WeatherNews } from './WeatherNews';
+import { WeatherNews } from './WeatherNews-completed';
 
 ReactDom.render(<WeatherNews />, document.getElementById('app'));
